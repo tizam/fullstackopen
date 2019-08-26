ab0d4b8f6ef3d639b669b0183e787513437cbd67
Ladoui Abdellah <bidam@pop-os.localdomain>
2019-08-26 12:33:38 +0100

    part one step two done

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -13,9 +13,10 @@ const App = () => {
   return (
     <>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content
+        part={[part1, part2, part3]}
+        exercises={[exercises1, exercises2, exercises3]}
+      />
       <Total exercises={exercises1 + exercises2 + exercises3} />
     </>
   );
@@ -30,6 +31,17 @@ const Header = props => {
 };
 
 const Content = props => {
+  console.log(props);
+  return (
+    <>
+      <Part part={props.part[0]} exercises={props.exercises[0]} />
+      <Part part={props.part[1]} exercises={props.exercises[1]} />
+      <Part part={props.part[2]} exercises={props.exercises[2]} />
+    </>
+  );
+};
+
+const Part = props => {
   return (
     <>
       <p>
