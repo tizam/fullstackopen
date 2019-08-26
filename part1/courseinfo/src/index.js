e4f2134c9b3a72dfb88129dc9a4ba04f214b62be
Ladoui Abdellah <ladoui@gmail.com>
2019-08-26 14:46:41 +0100

    part one step four done

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -4,36 +4,31 @@ import ReactDOM from "react-dom";
 const App = () => {
   const course = "Half Stack application development";
 
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10
-  };
-
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7
-  };
-
-  const part3 = {
-    name: "State of component",
-    exercises: 14
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7
+    },
+    {
+      name: "State of component",
+      exercises: 14
+    }
+  ];
 
   return (
     <>
       <Header course={course} />
-      <Content
-        part={[part1.name, part2.name, part3.name]}
-        exercises={[part1.exercises, part2.exercises, part3.exercises]}
-      />
-      <Total exercises={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </>
   );
 };
 
 const Header = props => {
-  console.log(props);
-
   return (
     <>
       <h1>{props.course}</h1>
@@ -42,30 +37,38 @@ const Header = props => {
 };
 
 const Content = props => {
-  console.log(props);
+  // console.log(props);
   return (
     <>
-      <Part part={props.part[0]} exercises={props.exercises[0]} />
-      <Part part={props.part[1]} exercises={props.exercises[1]} />
-      <Part part={props.part[2]} exercises={props.exercises[2]} />
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
     </>
   );
 };
 
 const Part = props => {
+  console.log(props);
+
   return (
     <>
       <p>
-        {props.part} {props.exercises}
+        {props.name} {props.exercises}
       </p>
     </>
   );
 };
 
 const Total = props => {
+  console.log(props);
   return (
     <>
-      <p>Number of exercises {props.exercises}</p>
+      <p>
+        Number of exercises{" "}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}
+      </p>
     </>
   );
 };
