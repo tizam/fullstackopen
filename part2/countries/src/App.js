62542633a189a362d0de259701777a8a922bf0a0
Ladoui Abdellah <ladoui@gmail.com>
2019-09-09 14:28:24 +0100

    part exercise 12 countries step 1 done

---
+++ part2/countries/src/App.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [countries, setCountries] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios.get("https://restcountries.eu/rest/v2/all").then(response => {
+      console.log(response.data);
+      setCountries(response.data);
+    });
+  }, []);
+
+  const handleFilter = event => {
+    setFilter(event.target.value);
+  };
+
+  const filteredCountries = countries.filter(country =>
+    country.name.toLowerCase().includes(filter)
+  );
+
+  const renderFilteredCountries = () => {
+    if (filteredCountries.length > 10) {
+      return <p>Too many matches, specify another filter</p>;
+    } else if (filteredCountries.length <= 10 && filteredCountries.length > 1) {
+      return filteredCountries.map(country => (
+        <p key={country.alpha2Code}>{country.name}</p>
+      ));
+    } else if (filteredCountries.length <= 1) {
+      return filteredCountries.map(country => (
+        <div key={country.callingCodes}>
+          <h3>{country.name}</h3>
+          <p>{country.capital}</p>
+          <div>
+            {country.population}
+            <p>{country.capital}</p>
+            <p>{country.population}</p>
+            <h4>languages</h4>
+            <ul>
+              {country.languages.map(language => (
+                <li key={language.iso639_1}>{language.name}</li>
+              ))}
+            </ul>
+            <p>
+              <img src={country.flag} alt={country.name} />
+            </p>
+          </div>
+        </div>
+      ));
+    }
+  };
+
+  return (
+    <div>
+      <p>
+        find countries <input onChange={handleFilter} />
+      </p>
+      <div>{renderFilteredCountries()}</div>
+    </div>
+  );
+}
+
+export default App;
